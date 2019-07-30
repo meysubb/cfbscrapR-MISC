e50d185e5adc1b9420a7ef2addca3219affd991d
Meyappan <meysubb@gmail.com>
2019-07-29 22:27:41 -0500

    update to the cleaning function, integral for the futures

--- EPA_WPA/2-EPA-Model.R
+++ EPA_WPA/2-EPA-Model.R
@@ -29,60 +29,31 @@ ep_model <- nnet::multinom(Next_Score ~ TimeSecsRem + adj_yd_line +
                              down + log_ydstogo + log_ydstogo*down +
                              adj_yd_line*down, data = pbp_no_OT, maxit = 300)
 
-test = pbp_no_OT %>% select(TimeSecsRem,down,distance,adj_yd_line,log_ydstogo)
-ep_start = predict(ep_model,test,type='prob')
-test = cbind(test,ep_start)
+
+test = pbp_no_OT %>% select(TimeSecsRem,down,distance,adj_yd_line,log_ydstogo) %>% 
+  filter()
+
+tamu_18 = pbp_no_OT %>% filter(
+  year == 2018,
+  offense %in% c("Clemson", "Texas A&M"),
+  defense %in% c("Clemson", "Texas A&M")
+) 
+
+tamu_18_pred = tamu_18 %>% select(TimeSecsRem,down,distance,adj_yd_line,log_ydstogo)
+
+ep_start = predict(ep_model,tamu_18_pred,type='prob')
+weights = c(0,3,-3,-2,-7,2,7)
+tamu_18_pred$ep_before = apply(ep_start, 1, function(row){
+  sum(row * weights)
+})
+#test = cbind(tamu_18,ep_start)
 
 # New Down = 1
 # New Distance = 10
 
+tamu_18_dat = prep_df_epa(tamu_18)
 
 
-prep_df_epa <- function(dat){
-  turnover_play_type = c(
-    'Fumble Recovery (Opponent)',
-    'Pass Interception Return',
-    'Interception Return Touchdown',
-    'Fumble Return Touchdown',
-    'Safety',
-    'Interception',
-    'Pass Interception'
-  )
-
-  dat = dat %>%
-    mutate_at(vars(clock.minutes, clock.seconds), ~ replace_na(., 0)) %>%
-    mutate(
-      play_text = tolower(play_text),
-      clock.minutes = ifelse(period %in% c(1, 3), 15 + clock.minutes, clock.minutes),
-      raw_secs = clock.minutes * 60 + clock.seconds,
-      coef = home_team == defense,
-      adj_yd_line = 100 * (1 - coef) + (2 * coef - 1) * yard_line,
-      log_ydstogo = log(adj_yd_line),
-      half = ifelse(period <= 2, 1, 2),
-      new_yardline = 0,
-      new_down = 0,
-      new_distance = 0,
-      turnover = 0
-  ) %>% select(-coef)
-
-  # Turnover Index
-  turnover_ind = dat$play_type %in% turnover_play_type
-  dat[turnover_ind,"new_down"] = 1
-  dat[turnover_ind,"new_distance"] = 10
-  # First down
-  first_down_ind = str_detect(pbp_no_OT$play_text,'1st')
-  dat[first_down_ind,"new_down"] = 1
-  dat[first_down_ind,"new_distance"] 10
-  # Otherwise What happened?
-  dat[(!turnover_ind & !first_down_ind),"new_down"] = dat[(!turnover_ind & !first_down_ind),"down"] + 1
-  dat[(!turnover_ind & !first_down_ind),"new_distance"] = dat[(!turnover_ind & !first_down_ind),"distance"] - dat[(!turnover_ind & !first_down_ind),"yards_gained"]
-
-
-  fifty_ydline = str_detect(pbp_no_OT$play_text,"50 yard line")
-  dat[fifty_ydline,"new_yardline"] = 50
-
-
-}
 
 
 ## pick an SEC game
--- EPA_WPA/6-Utils.R
+++ EPA_WPA/6-Utils.R
@@ -48,3 +48,73 @@ find_next_score <- function(play_i,score_plays_i,dat_drive){
                       DSH = score_drive))
   }
 }
+
+prep_df_epa <- function(dat) {
+  # This function is used to calculate the EP - after the play
+  # Then EPA = EP_After - EP_Before 
+  # Provide, this function so people can calcualte 
+  # EPA on new games in 2019 szn
+  # after which they can feed that to the WPA model
+  turnover_play_type = c(
+    'Fumble Recovery (Opponent)',
+    'Pass Interception Return',
+    'Interception Return Touchdown',
+    'Fumble Return Touchdown',
+    'Safety',
+    'Interception',
+    'Pass Interception'
+  )
+  
+  dat = dat %>%
+    mutate_at(vars(clock.minutes, clock.seconds), ~ replace_na(., 0)) %>%
+    mutate(
+      play_text = tolower(play_text),
+      clock.minutes = ifelse(period %in% c(1, 3), 15 + clock.minutes, clock.minutes),
+      raw_secs = clock.minutes * 60 + clock.seconds,
+      coef = home_team == defense,
+      adj_yd_line = 100 * (1 - coef) + (2 * coef - 1) * yard_line,
+      log_ydstogo = log(adj_yd_line),
+      half = ifelse(period <= 2, 1, 2),
+      new_yardline = 0,
+      new_down = 0,
+      new_distance = 0,
+      turnover = 0
+    ) %>% select(-coef)
+  
+  # Turnover Index
+  turnover_ind = dat$play_type %in% turnover_play_type
+  dat[turnover_ind, "new_down"] = 1
+  dat[turnover_ind, "new_distance"] = 10
+  # First down
+  first_down_ind = str_detect(dat$play_text, '1st')
+  dat[first_down_ind, "new_down"] = 1
+  dat[first_down_ind, "new_distance"] = 10
+  dat[first_down_ind, "new_yardline"] = dat[first_down_ind,"adj_yd_line"] - dat[first_down_ind,"yards_gained"]
+  # Otherwise What happened?
+  dat[(!turnover_ind &
+         !first_down_ind), "new_down"] = dat[(!turnover_ind &
+                                                !first_down_ind), "down"] + 1
+  dat[(!turnover_ind &
+         !first_down_ind), "new_distance"] = dat[(!turnover_ind &
+                                                    !first_down_ind), "distance"] - dat[(!turnover_ind &
+                                                                                           !first_down_ind), "yards_gained"]
+  dat[(!turnover_ind & !first_down_ind),"new_yardline"] =  dat[(!turnover_ind &
+                                                                  !first_down_ind), "adj_yd_line"] - dat[(!turnover_ind &
+                                                                                                            !first_down_ind), "yards_gained"]
+  
+  
+  fifty_ydline = str_detect(dat$play_text, "50 yard line")
+  dat[fifty_ydline, "new_yardline"] = 50
+  
+  opp_fumb_rec = str_detect(dat$play_text, "Fumble Recovery (Opponent)")
+  dat[opp_fumb_rec, "new_yardline"] = 100 - (dat[opp_fumb_rec, "yard_line"] +
+                                               dat[opp_fumb_rec, "yards_gained"])
+  dat[opp_fumb_rec, "new_down"] = 1
+  dat[opp_fumb_rec, "new_distance"] = 10
+  
+  sack = str_detect(dat$play_type, "Sack")
+  dat[sack, "new_yardline"] = 100 - (dat[sack, "yard_line"] - dat[sack, "yards_gained"])
+  dat[sack, "new_down"] = dat[sack, "down"] + 1
+  dat[sack, "new_distance"] = dat[sack, "distance"] - dat[sack, "yards_gained"]
+  return(dat)
+}
