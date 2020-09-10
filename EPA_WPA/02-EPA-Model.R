d6cad3c587e25b27886502536c1b09787f34b45b
saiemgilani <saiem.gilani@gmail.com>
2020-09-10 04:22:11 -0400

    Update 02-EPA-Model.R
    update plots with theming and calibration error caption

--- EPA_WPA/02-EPA-Model.R
+++ EPA_WPA/02-EPA-Model.R
@@ -86,52 +86,6 @@ ep_cv_loso_calibration_results <-
             n_scoring_event = length(which(Next_Score == next_score_type)),
             bin_actual_prob = n_scoring_event / n_plays)
 
-
-# Create a label data frame for the chart:
-ann_text <- data.frame(x = c(.25, 0.75), y = c(0.75, 0.25), 
-                       lab = c("More times\nthan expected", "Fewer times\nthan expected"),
-                       next_score_type = factor("No Score (0)"))
-
-# # Create the calibration chart:
-# ep_cv_loso_calibration_results %>%
-#   ungroup() %>%
-#   mutate(next_score_type = fct_relevel(next_score_type,
-#                                        "Opp_Safety", "Opp_FG",
-#                                        "Opp_TD", "No_Score", "Safety",
-#                                        "FG", "TD"
-#   ),
-#   next_score_type = fct_recode(next_score_type,
-#                                "-Field Goal (-3)" = "Opp_FG",
-#                                "-Safety (-2)" = "Opp_Safety",
-#                                "-Touchdown (-7)" = "Opp_TD",
-#                                "Field Goal (3)" = "FG",
-#                                "No Score (0)" = "No_Score",
-#                                "Touchdown (7)" = "TD",
-#                                "Safety (2)" = "Safety")) %>%
-#   ggplot() +
-#   geom_point(aes(x = bin_pred_prob, y = bin_actual_prob, size = n_plays)) +
-#   geom_smooth(aes(x = bin_pred_prob, y = bin_actual_prob), method = "loess") +
-#   geom_abline(slope = 1, intercept = 0, color = "black", lty = 2) +
-#   coord_equal() +   geom_text(data = ann_text,aes(x = x, y = y, label = lab)) +
-#   scale_x_continuous(limits = c(0,1)) +
-#   scale_y_continuous(limits = c(0,1)) +
-#   labs(size = "Number of plays",
-#        x = "Estimated next score probability",
-#        y = "Observed next score probability") +
-#   geom_text(data = ann_text, aes(x = x, y = y, label = lab)) +
-#   theme_bw() +
-#   theme(plot.title = element_text(hjust = 0.5),
-#         strip.background = element_blank(),
-#         strip.text = element_text(size = 18),
-#         axis.title = element_text(size = 18),
-#         axis.text.y = element_text(size = 12),
-#         axis.text.x = element_text(size = 10, angle = 90),
-#         legend.title = element_text(size = 18),
-#         legend.text = element_text(size = 16),
-#         legend.position = c(1, .05), legend.justification = c(1, 0)) +
-#   facet_wrap(~ next_score_type, ncol = 4)+
-#   ggsave("ep_cv_loso_calibration_results.png", height = 9/1.2, width = 16/1.2)
-
 # Calculate the calibration error values:  
 cv_cal_error <- ep_cv_loso_calibration_results %>% 
   ungroup() %>%
@@ -140,9 +94,83 @@ cv_cal_error <- ep_cv_loso_calibration_results %>%
   summarize(weight_cal_error = weighted.mean(cal_diff, n_plays, na.rm = TRUE),
             n_scoring_event = sum(n_scoring_event, na.rm = TRUE))
 
+# Calculate the calibration error values:
+cv_cal_error <- ep_cv_loso_calibration_results %>%
+  ungroup() %>%
+  mutate(cal_diff = abs(bin_pred_prob - bin_actual_prob)) %>%
+  group_by(next_score_type) %>%
+  summarize(weight_cal_error = weighted.mean(cal_diff, n_plays, na.rm = TRUE),
+            n_scoring_event = sum(n_scoring_event, na.rm = TRUE))
+
 # Overall weighted calibration error:
+print("Weighted calibration error:")
 with(cv_cal_error, weighted.mean(weight_cal_error, n_scoring_event))
-# 0.01103588
+# new: 0.01103588, old: 0.01123694
+cal_error_no_FG <- round(with(cv_cal_error, weighted.mean(weight_cal_error, n_scoring_event)),5)
+
+plot_caption <- glue::glue("Overall Weighted Calibration Error: {cal_error_no_FG}")
+
+# Create a label data frame for the chart:
+ann_text <- data.frame(x = c(.25, 0.75), y = c(0.75, 0.25), 
+                       lab = c("More times\nthan expected", "Fewer times\nthan expected"),
+                       next_score_type = factor("No Score (0)"))
+
+# # Create the calibration chart:
+ep_cv_loso_calibration_results %>% 
+  ungroup() %>% 
+  mutate(next_score_type = fct_relevel(next_score_type,
+                                       "Opp_FG",
+                                       "Opp_Safety","Opp_TD",
+                                       "FG","Safety","TD",
+                                       "No_Score"),
+         next_score_type = fct_recode(next_score_type,
+                                      "-Field Goal (-3)" = "Opp_FG",
+                                      "-Safety (-2)" = "Opp_Safety",
+                                      "-Touchdown (-7)" = "Opp_TD",
+                                      "Field Goal (3)" = "FG",
+                                      "Touchdown (7)" = 'TD',
+                                      "No Score (0)" = "No_Score")) %>%
+  ggplot() +
+  geom_point(aes(x = bin_pred_prob, y = bin_actual_prob, size = n_plays))+
+  geom_smooth(aes(x = bin_pred_prob, y = bin_actual_prob), method = "loess") +
+  geom_abline(slope = 1, intercept = 0, color = "black", lty = 2) +
+  coord_equal() +   geom_text(data = ann_text,aes(x = x, y = y, label = lab)) +
+  scale_x_continuous(limits = c(0,1)) + 
+  scale_y_continuous(limits = c(0,1)) + 
+  labs(title = "Calibration plots for Expected Points Model by Scoring Event",
+       subtitle = "Leave-One-Season-Out Cross-Validation, EP-only Model",
+       caption = plot_caption,
+       size = "Number of plays",
+       x = "Estimated Next Score Probability",
+       y = "Observed Next Score Probability") + 
+  geom_text(data = ann_text, aes(x = x, y = y, label = lab)) +
+  theme(
+    legend.title = element_text(size = 9, margin=margin(t=0.2,r=0,b=0.2,l=0.2,unit=c("mm")), family = "serif",face = "bold"),
+    legend.text = element_text(size = 9, margin=margin(t=0.2,r=0,b=0.2,l=0.2,unit=c("mm")), family = "serif"),
+    legend.background = element_rect(fill = "grey99"),
+    legend.key = element_rect(fill = "grey99"),
+    legend.key.width = unit(1.5,"mm"),
+    legend.key.size = unit(2.0,"mm"),
+    legend.margin=margin(t = 0.4,b = 0.4,l=0.4,r=0.4,unit=c('mm')),
+    legend.position = c(0.90, 0.22),
+    legend.direction = "vertical",
+    legend.box.background = element_rect(colour = "#500f1b"),
+    axis.title.x = element_text(size = 10, margin=margin(0,0,0,0, unit=c("mm")), family = "serif", face = "bold"),
+    axis.text.x = element_text(size = 10, margin=margin(0,0,-0.1,0, unit=c("mm")), family = "serif"),
+    axis.title.y = element_text(size = 10, margin=margin(0,0,0,0, unit=c("mm")), family = "serif", face = "bold"),
+    axis.text.y = element_text(size = 10, margin=margin(0,0.1,0,0, unit=c("mm")), family = "serif"),
+    plot.title = element_text(size = 12, margin=margin(t=0, r=0, b=0.2,l=0, unit=c("mm")), 
+                              lineheight=0.7, family = "serif", face = "bold", hjust = 0.5),
+    plot.subtitle = element_text(size = 10, margin=margin(t=0, r=0, b=0.2,l=0, unit=c("mm")), 
+                                 lineheight=0.7, family = "serif", hjust = 0.5),
+    plot.caption = element_text(size = 10, margin=margin(t=0.5, r=0, b=0,l=0, unit=c("mm")), lineheight=0.7, family = "serif", face = "bold"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "grey85", color = "black"),
+    plot.background = element_rect(fill = "grey99", color = "black"),
+    plot.margin=unit(c(1,2,1,3),"mm"))+      
+  facet_wrap(~ next_score_type, ncol = 4)+  
+  ggsave("figures/ep_cv_loso_calibration_results.png", height = 9/1.2, width = 16/1.2)
 
 # Create the LOSO predictions for the selected cfbscrapR models:
 ep_fg_model_loso_preds <- calc_ep_multinom_fg_loso_cv(as.formula("Next_Score ~ 
@@ -176,14 +204,29 @@ ep_fg_cv_loso_calibration_results <- ep_fg_model_preds %>%
             n_scoring_event = length(which(Next_Score == next_score_type)),
             bin_actual_prob = n_scoring_event / n_plays)
 
+# Calculate the calibration error values:  
+cv_fg_cal_error <- ep_fg_cv_loso_calibration_results %>% 
+  ungroup() %>%
+  mutate(cal_diff = abs(bin_pred_prob - bin_actual_prob)) %>%
+  group_by(next_score_type) %>% 
+  summarize(weight_cal_error = weighted.mean(cal_diff, n_plays, na.rm = TRUE),
+            n_scoring_event = sum(n_scoring_event, na.rm = TRUE))
+
+# Overall weighted calibration error:
+print("Weighted calibration error:")
+cal_error <- round(with(cv_fg_cal_error, weighted.mean(weight_cal_error, n_scoring_event)),5)
+print(cal_error)
+# new: 0.01307456, old: 0.01314949
+
+plot_caption <- glue::glue("Overall Weighted Calibration Error: {cal_error}")
 
 # Create a label data frame for the chart:
 ann_text <- data.frame(x = c(.25, 0.75), y = c(0.75, 0.25), 
                        lab = c("More times\nthan expected", "Fewer times\nthan expected"),
                        next_score_type = factor("No Score (0)"))
 
-# Create the calibration chart:
-ep_fg_cv_loso_calibration_results %>%
+# # Create the calibration chart:
+ep_fg_cv_loso_calibration_results %>% 
   ungroup() %>%
   mutate(next_score_type = fct_relevel(next_score_type,
                                        "Opp_FG",
@@ -198,35 +241,47 @@ ep_fg_cv_loso_calibration_results %>%
                                       "Touchdown (7)" = 'TD',
                                       "No Score (0)" = "No_Score")) %>%
   ggplot() +
-  geom_point(aes(x = bin_pred_prob, y = bin_actual_prob, size = n_plays)) +
+  geom_point(aes(x = bin_pred_prob, y = bin_actual_prob, size = n_plays))+
   geom_smooth(aes(x = bin_pred_prob, y = bin_actual_prob), method = "loess") +
   geom_abline(slope = 1, intercept = 0, color = "black", lty = 2) +
   coord_equal() +   geom_text(data = ann_text,aes(x = x, y = y, label = lab)) +
   scale_x_continuous(limits = c(0,1)) + 
   scale_y_continuous(limits = c(0,1)) + 
-  labs(title = "Leave-One-Season-Out Cross Validation Calibration for\nExpected Points Model by Scoring Event",
+  labs(title = "Calibration plots for Expected Points Model by Scoring Event",
+       subtitle = "Leave-One-Season-Out Cross-Validation, EP & FG Model",
+       caption = plot_caption,
        size = "Number of plays",
        x = "Estimated Next Score Probability",
        y = "Observed Next Score Probability") + 
-  #scale_color_brewer(palette = "Spectral", name = NULL, guide = FALSE) +
   geom_text(data = ann_text, aes(x = x, y = y, label = lab)) +
-  theme_bw() + 
-  theme(plot.title = element_text(hjust = 0.5)) +
-  facet_wrap(~ next_score_type, ncol = 3)+  
-  ggsave("ep_fg_cv_loso_calibration_results.png", height = 9/1.2, width = 16/1.2)
-
-
-# Calculate the calibration error values:  
-cv_fg_cal_error <- ep_fg_cv_loso_calibration_results %>% 
-  ungroup() %>%
-  mutate(cal_diff = abs(bin_pred_prob - bin_actual_prob)) %>%
-  group_by(next_score_type) %>% 
-  summarize(weight_cal_error = weighted.mean(cal_diff, n_plays, na.rm = TRUE),
-            n_scoring_event = sum(n_scoring_event, na.rm = TRUE))
-
-# Overall weighted calibration error:
-with(cv_fg_cal_error, weighted.mean(weight_cal_error, n_scoring_event))
-# 0.01307456
+  theme(
+    legend.title = element_text(size = 9, margin=margin(t=0.2,r=0,b=0.2,l=0.2,unit=c("mm")), family = "serif",face = "bold"),
+    legend.text = element_text(size = 9, margin=margin(t=0.2,r=0,b=0.2,l=0.2,unit=c("mm")), family = "serif"),
+    legend.background = element_rect(fill = "grey99"),
+    legend.key = element_rect(fill = "grey99"),
+    legend.key.width = unit(1.5,"mm"),
+    legend.key.size = unit(2.0,"mm"),
+    legend.margin=margin(t = 0.4,b = 0.4,l=0.4,r=0.4,unit=c('mm')),
+    legend.position = c(0.90, 0.22),
+    legend.direction = "vertical",
+    legend.box.background = element_rect(colour = "#500f1b"),
+    axis.title.x = element_text(size = 12, margin=margin(0,0,0,0, unit=c("mm")), family = "serif", face = "bold"),
+    axis.text.x = element_text(size = 12, margin=margin(0,0,-0.1,0, unit=c("mm")), family = "serif"),
+    axis.title.y = element_text(size = 12, margin=margin(0,0,0,0, unit=c("mm")), family = "serif", face = "bold"),
+    axis.text.y = element_text(size = 12, margin=margin(0,0.1,0,0, unit=c("mm")), family = "serif"),
+    plot.title = element_text(size = 12, margin=margin(t=0, r=0, b=0.2,l=0, unit=c("mm")), 
+                              lineheight=0.7, family = "serif", face = "bold", hjust = 0.5),
+    plot.subtitle = element_text(size = 10, margin=margin(t=0, r=0, b=0.2,l=0, unit=c("mm")), 
+                                 lineheight=0.7, family = "serif", hjust = 0.5),
+    plot.caption = element_text(size = 10, margin=margin(t=0.3, r=0, b=0,l=0, unit=c("mm")), 
+                                lineheight=0.7, family = "serif", face = "bold"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "grey85", color="black"),
+    plot.background = element_rect(fill = "grey99", color="black"),
+    plot.margin=unit(c(1,2,1,3),"mm"))+      
+  facet_wrap(~ next_score_type, ncol = 4)+  
+  ggsave("figures/ep_fg_cv_loso_calibration_results.png", height = 9/1.2, width = 16/1.2)
 
 ### Create Final Models 
 final_pbp = pbp_no_OT %>% mutate(
@@ -259,7 +314,7 @@ saveRDS(ep_model,file="models/final_ep_model.RDS")
 # need to separate them
 pbp_fg_df <- pbp_full_df %>% 
   filter(year>=2014) %>%   
-  filter(grepl("Field Goal",play_type) | grepl("Extra Point",play_type)) %>%
+  filter(grepl("Field Goal", play_type) | grepl("Extra Point", play_type)) %>%
   filter(!is.na(down),!is.na(raw_secs)) %>%
   filter(!is.na(game_id)) %>%
   filter(log_ydstogo != -Inf) %>%
@@ -283,8 +338,6 @@ save(fg_model,file="models/final_fg_model.RData")
 
 all_years = pbp_no_OT %>% split(pbp_no_OT$year)
 
-
-#source("08-Pred-Utils.R")
 all_years_epa = lapply(all_years, function(x) {
   year = unique(x$year)
   print(year)
@@ -292,7 +345,7 @@ all_years_epa = lapply(all_years, function(x) {
   x = clean_pbp_dat(x)
   x = penalty_detection(x)
   #browser()
-  val =create_epa(x,ep_model = ep_model,fg_model = fg_model)
+  val = create_epa(x, ep_model = ep_model, fg_model = fg_model)
   return(val)
 })
 
@@ -300,8 +353,30 @@ len = length(all_years)
 
 for (i in 1:len) {
   df = all_years_epa[[i]]
-  hist(df$EPA)
-  Sys.sleep(5)
+  year = df$year[1]
+  filename = glue::glue("figures/histogram_epa_season_{year}.png")
+  plot_title = glue::glue("Histogram of EPA - {year} season")
+  ggplot(df, aes(x = EPA))+
+    geom_histogram(color="darkblue", fill="lightblue", binwidth=1)+
+    labs(title = plot_title,
+         subtitle = "Data: @CFB_data with #cfbscrapR",
+         x = "Expected Points Added", 
+         y = "Number of plays") +
+    theme(
+      axis.title.x = element_text(size = 10, margin=margin(0,0,0,0, unit=c("mm")), family = "serif", face = "bold"),
+      axis.text.x = element_text(size = 10, margin=margin(0,0,-0.1,0, unit=c("mm")), family = "serif"),
+      axis.title.y = element_text(size = 10, margin=margin(0,0,0,0, unit=c("mm")), family = "serif", face = "bold"),
+      axis.text.y = element_text(size = 10, margin=margin(0,0.1,0,0, unit=c("mm")), family = "serif"),
+      plot.title = element_text(size = 12, margin=margin(t=0, r=0, b=0.2,l=0, unit=c("mm")), lineheight=0.7, family = "serif", face = "bold"),
+      plot.subtitle = element_text(size = 10, margin=margin(t=0, r=0, b=0.2,l=0, unit=c("mm")), lineheight=0.7, family = "serif"),
+      plot.caption = element_text(size = 10, margin=margin(t=0, r=0, b=0,l=0, unit=c("mm")), lineheight=0.7, family = "serif"),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(fill = "grey35", color="black"),
+      plot.background = element_rect(fill = "grey85", color="black"),
+      plot.margin=unit(c(1,2,1,3),"mm"))+
+    ggsave(filename, height = 101.6, width = 152.4, units=c('mm'), type="cairo")
+  Sys.sleep(1)
 }
 
 
@@ -309,5 +384,5 @@ lapply(all_years_epa,function(x){
   year = unique(x$year)
   name = glue::glue("data/rds/EPA_FG_Calcs_{year}.RDS")
   print(name)
-  saveRDS(x,name)
+  saveRDS(x, name)
 })
